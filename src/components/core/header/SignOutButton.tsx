bd5cc9079d15c80358ead7d7356c5f6080beb67f
Vinicius <dev.viniciusmendes@gmail.com>
2025-03-21 23:35:21 -0300

    ajustes

--- src/components/core/header/SignOutButton.tsx
+++ src/components/core/header/SignOutButton.tsx
@@ -1,10 +1,9 @@
 "use client";
-import { signOut } from "next-auth/react";
 import { LogOut } from "lucide-react";
 
 export const SignOutButton = () => {
   return (
-    <button onClick={() => signOut()}>
+    <button >
       <span className="flex gap-1 items-center">
         <LogOut className="h-4" /> Sign out
       </span>
